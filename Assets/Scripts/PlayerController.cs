c20d465d3098677d89f6c96534ddcfd415040824
HinaMatsuzaki <hmatsuzaki@knox.edu>
2022-10-09 21:15:40 +0100

    added pickups

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,12 @@ public class PlayerController : MonoBehaviour {
 
     public Vector2 moveValue;
     public float speed;
+    private int count;
+
+    private void Start()
+    {
+        count = 0;
+    }
 
     void OnMove ( InputValue value ) {
         moveValue = value.Get<Vector2>();
@@ -15,5 +21,13 @@ public class PlayerController : MonoBehaviour {
     void FixedUpdate () {
         Vector3 movement = new Vector3 ( moveValue.x , 0.0f , moveValue.y );
         GetComponent < Rigidbody >().AddForce ( movement * speed * Time.fixedDeltaTime );
+    }
+
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.tag == "PickUp"){
+            other.gameObject.SetActive(false);
+            count++;
+            Debug.Log(count);
+        }
     }
 }
