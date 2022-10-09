10efe5136e6f8aec7240031060ea28f07a8f3c00
HinaMatsuzaki <hmatsuzaki@knox.edu>
2022-10-09 21:49:24 +0100

    added score UI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,16 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour {
 
     public Vector2 moveValue;
     public float speed;
     private int count;
+    private int numPickups = 3;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
 
     private void Start()
     {
         count = 0;
+        winText.text = "";
+        SetCountText();
     }
 
     void OnMove ( InputValue value ) {
@@ -27,7 +33,14 @@ public class PlayerController : MonoBehaviour {
         if (other.gameObject.tag == "PickUp"){
             other.gameObject.SetActive(false);
             count++;
-            Debug.Log(count);
+            SetCountText();
+        }
+    }
+
+    private void SetCountText(){
+        scoreText.text = " Score : " + count.ToString();
+        if (count >= numPickups){
+            winText.text = " You win ! ";
         }
     }
 }
